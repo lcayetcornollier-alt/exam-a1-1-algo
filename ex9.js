6d35826671d436453f988174e1cb58f9c0a0cd88
lcayetcornollier-alt <lcayetcornollier@edenschool.fr>
2025-11-12 12:00:33 +0100

    ss
    ss

--- ex9.js
+++ ex9.js
@@ -1,16 +1,16 @@
 // ex09.js
 
 function execute(username, steps, operation) {
-  // Écrire le code ici
+  return operation(username, steps);
 }
 
 function forward(username, steps) {
-  // Écrire le code ici
+  return username + " avance de " + steps;
 }
 
 function backward(username, steps) {
-  // Écrire le code ici
+  return username + " recule de " + steps;
 }
 
-execute("BenBest", 1, forward); // "BenBest avance de 1."
-execute("KingStar", 10, backward); // "KingStar recule de 10."
+console.log(execute("BenBest", 1, forward)); // "BenBest avance de 1."
+console.log(execute("KingStar", 10, backward)); // "KingStar recule de 10."
